0c5941f6a8b05695003ef04d27c0edbfd811f96e
adamos581 <adabelni@student.pg.gda.pl>
2017-12-27 16:44:53 +0100

    final result, just run

--- environment.py
+++ environment.py
@@ -1,5 +1,8 @@
 import numpy as np
 from numpy import random
+import copy
+import matplotlib.pyplot as plt
+
 
 class Environment:
 
@@ -31,12 +34,14 @@ def check_population(population, environment):
         sum_weight, sum_value = person.fit(environment)
         population_values.append(sum_value)
         population_weights.append(sum_weight)
+    return population_weights, population_values
 
 
 def create_parents(population, number_population):
     parents = []
     for i in range(number_population * 7):
-        parents.append(random.choice(population))
+        new_person = random.choice(population)
+        parents.append(copy.deepcopy(new_person))
     return parents
 
 
@@ -44,8 +49,8 @@ def uniform_crossover(parents, len_items):
     parents_crossover = []
 
     while len(parents) > 0:
-        first_person = parents.pop(random.random_integers(0, len(parents)))
-        second_person = parents.pop(random.random_integers(0, len(parents)))
+        first_person = parents.pop(random.random_integers(0, len(parents) - 1))
+        second_person = parents.pop(random.random_integers(0, len(parents) - 1))
 
         if random.rand() > 0.6:
             pattern = random.random_integers(0, 1, len_items)
@@ -56,8 +61,9 @@ def uniform_crossover(parents, len_items):
                     second_person.backpack[i] = r
 
             alfa = random.rand()
-            new_first_deviation = alfa * first_person.standard_deviation + (1 - alfa) * second_person.standard_deviation
-            new_second_deviation = alfa * second_person.standard_deviation + (1 - alfa) * first_person.standard_deviation
+
+            new_first_deviation = alfa * np.array(first_person.standard_deviation) + (1 - alfa) * np.array(second_person.standard_deviation)
+            new_second_deviation = alfa * np.array(second_person.standard_deviation) + (1 - alfa) * np.array(first_person.standard_deviation)
             first_person.standard_deviation = new_first_deviation
             second_person.standard_deviation = new_second_deviation
 
@@ -67,10 +73,107 @@ def uniform_crossover(parents, len_items):
     return parents_crossover
 
 
-# def mutation(parents):
+def mutation(parents, step):
 
+    for parent in parents:
+        teta = random.normal()
+        tau = np.sqrt(np.sqrt(step + 1) * 2) ** -1
+        v = np.sqrt(1 + step * 2) ** -1
+        deviation_vector = parent.standard_deviation
+        new_deviation = []
+        for deviation in deviation_vector:
+            teta_i = random.normal()
+            deviation = deviation * (np.e ** (tau * teta + v * teta_i))
+            new_deviation.append(deviation)
+
+        parent.standard_deviation = new_deviation
+        backpack_vector = parent.backpack
+        new_backpack = []
+
+        for backpack, deviation in zip(backpack_vector, new_deviation):
+            eta_i = random.normal()
+            change =  eta_i * deviation
+            if abs(change) >= 0.5:
+                if backpack == 1:
+                    backpack = 0
+                else:
+                    backpack = 1
+            new_backpack.append(backpack)
+        parent.backpack = new_backpack
+    return parents
+
+
+def get_best(population, environment, number_population):
+    population_weights, population_values = check_population(population, environment)
+
+    well_weight = []
+    for person, weight, value in zip(population, population_weights, population_values):
+        if weight <= environment.capacity:
+            well_weight.append((person, value, weight))
+
+    if len(well_weight) == 0:
+        for person, weight, value in zip(population, population_weights, population_values):
+            well_weight.append((person, value, weight))
+        new_population = sorted(well_weight, key=lambda person: person[2])
+        new_population, values, weights = zip(*new_population)
+        new_population = list(new_population)
+
+        if len(new_population) > number_population:
+            avaregae_values = np.sum(values[:number_population]) / number_population
+            avaregae_weight = np.sum(weights[:number_population]) / number_population
+            std_values = np.std(values)
+            return new_population[:number_population], avaregae_values, avaregae_weight, std_values
+
+    new_population = sorted(well_weight, key=lambda person: person[1])
+    new_population, values, weights = zip(*new_population)
+    new_population = list(new_population)
+
+    if len(new_population) > number_population:
+        avaregae_values = np.sum(values[-number_population:]) / number_population
+        avaregae_weight = np.sum(weights[-number_population:]) / number_population
+        std_values = np.std(values)
+
+        return new_population[-number_population:], avaregae_values, avaregae_weight, std_values
+
+    std_values = np.std(values)
+    return new_population, np.sum(values) / len(values), np.sum(weights) / len(weights), std_values
 
 
 if __name__ == '__main__':
-    number_population = 20
-    population = [Species(150) for i in range(number_population)]
+    len_items = 150
+    # population = [Species(len_items) for i in range(number_population)]
+    environment = Environment(len_items, 400)
+
+    plt.figure(1)
+
+    for i in range(3):
+        number_population = 40
+
+        population = [Species(len_items) for i in range(number_population)]
+
+        average_value = []
+        average_weights = []
+        std_values = []
+        for i in range(35):
+            parents = create_parents(population, number_population)
+            parents_crossover = uniform_crossover(parents, len_items)
+            parents_mutation = mutation(parents_crossover, i)
+            population = population + parents_mutation
+            population = get_best(population, environment, number_population)
+            average_value.append(population[1])
+            average_weights.append(population[2])
+            std_values.append(population[3])
+
+            population = population[0]
+            print (i)
+
+        plt.subplot(211 )
+
+        plt.errorbar(range(len(average_value)),average_value,  yerr=std_values)
+        plt.ylabel('wartosc plecaka')
+        plt.subplot(212)
+        plt.plot(average_weights)
+        plt.ylabel('waga plecaka')
+
+
+    plt.show()
